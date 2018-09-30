76d01f324b731e3e1f77746a660281a356562182
Mudathir Lawal <al-mudathir@live.com>
2018-09-30 20:35:39 +0100

    Ensuring variables are properly named.

--- PowersOfTwo.c
+++ PowersOfTwo.c
@@ -28,16 +28,16 @@ void PrintPowersOfTwo ( void )
 	const double TWO = 2.00000;
 	const double MAX_POWER_OF_TWO = 65536.00000;
 	double powerOfTwo = FIRST_POWER_OF_TWO;
-	double fpower = 0.00000;
+	double power = 0.00000;
 	
 	printf ( "\n\n\t\tPOWERS OF TWO: 1 - 65536" );
 	printf ( "\n\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ, EQ );
 
 	while ( powerOfTwo <= MAX_POWER_OF_TWO )
 	{
-		printf ( "\n\t\t%.0f%c%c%c%.0f\t%c%c%c\t%.0f", TWO, ES, CR, ES, fpower, EQ, ES, ES, powerOfTwo );
+		printf ( "\n\t\t%.0f%c%c%c%.0f\t%c%c%c\t%.0f", TWO, ES, CR, ES, power, EQ, ES, ES, powerOfTwo );
 		powerOfTwo = powerOfTwo * 2.0;
-		fpower = fpower + 1.0;
+		power = power + 1.0;
 	}	
 
 	printf ( "\n\n" );
