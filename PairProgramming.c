66abe96c11bebd0b56ad81dd3d5ad82dbed89070
Mudathir Lawal <al-mudathir@live.com>
2018-12-11 14:35:51 +0100

    Another Pair Programing Session

---
+++ PairProgramming.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+
+int GetIndexOfLargest ( int array [], int length )
+{
+	int largestElem = array [ 0 ];
+	int loopVar = 0;
+	int index;
+
+    do
+    {
+    	if ( largestElem < array [ loopVar ] )
+    	{
+	        largestElem = array [ loopVar ];
+	        index 		= loopVar;
+		}
+		loopVar++;
+    }
+    while ( loopVar < length );
+    
+    printf ( "\n" );
+    printf ( "\tMaxElement: %i; Index: %i\n\n", largestElem, index );
+	return index;
+}
+
+
+// My Main func
+int main ( void )
+{
+	int array [ 8 ] = { 99, 23, 16, 12, 14, 55, 22, 77 };
+	int length = sizeof ( array ) / sizeof ( array [ 6 ] );
+
+	GetIndexOfLargest ( array, length );
+	return EXIT_SUCCESS;
+}	
+
